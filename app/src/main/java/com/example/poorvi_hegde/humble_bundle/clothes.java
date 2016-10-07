3bdd83b4e262f6ef9b3b01034a6eaec717155925
PoorviHegde <poorviexam@gmail.com>
2016-10-07 21:23:47 +0530

    no changes

---
+++ app/src/main/java/com/example/poorvi_hegde/humble_bundle/clothes.java
@@ -0,0 +1,16 @@
+package com.example.poorvi_hegde.humble_bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Poorvi_Hegde on 05-10-2016.
+ */
+public class clothes extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clothes);
+    }
+}
